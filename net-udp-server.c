8d747a0748e2e5045f1bcb6d9b3f837c5d6df3b7
Michael Wuertinger <michael@wuertinger.de>
2019-01-07 20:29:32 +0100

    Usage information for net-udp-server

--- net-udp-server.c
+++ net-udp-server.c
@@ -7,6 +7,11 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+		exit(1);
+	}
+
 	int port = atoi(argv[1]);
 	struct sockaddr_in listenaddr;
 	memset(&listenaddr, 0, sizeof(struct sockaddr_in));
